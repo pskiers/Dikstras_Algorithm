339f92083a1704a8934ec7ac294429852d4a6c31
pawel <pawelsk.vp.pl@gmial.com>
2021-06-15 20:20:41 +0200

    add argprase and example graphs with pictures

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import argparse
 from dirstra import dikstra
 from read_map import read_form_file
 
-if __name__ == "__main__":
-    file_name = input('Enter name of file with the table: ')
+
+def main(file_name):
     graph = read_form_file(file_name)
     print(dikstra(graph))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help='enter file name')
+    args = parser.parse_args()
+    main(args.name)
